838f66aeb5a384305258bdad963238b0c05d5530
sleepycat <yourfather@163.com>
2020-07-27 22:23:19 +0800

    状态管理与更新dom

--- ToyReact.js
+++ ToyReact.js
@@ -3,13 +3,29 @@ class ElementWrapper {
     this.root = document.createElement(type);
   }
   setAttribute(name, value) {
+    if (name.match(/^on([\s\S]+)$/)) {
+      const eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase());
+      this.root.addEventListener(eventName, value);
+    }
+    if (name === 'className')
+      name = 'class';
     this.root.setAttribute(name, value);
   }
   appendChild(vchild) {
-    vchild.mountTo(this.root);
+    let range = document.createRange();
+    if (this.root.children.length) {
+      range.setStartAfter(this.root.lastChild);
+      range.setEndAfter(this.root.lastChild);
+    } else {
+      range.setStart(this.root, 0);
+      range.setEnd(this.root, 0);
+    }
+    vchild.mountTo(range);
   }
-  mountTo(parent) {
-    parent.appendChild(this.root);
+  mountTo(range) {
+    range.deleteContents();
+    range.insertNode(this.root);
+    // parent.appendChild(this.root);
   }
 }
 
@@ -17,31 +33,69 @@ class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content);
   }
-  mountTo(parent) {
-    parent.appendChild(this.root);
+  mountTo(range) {
+    range.deleteContents();
+    range.insertNode(this.root);
+    // parent.appendChild(this.root);
   }
 }
 
 export class Component {
   constructor() {
     this.children = [];
+    this.props = Object.create(null);
   }
   setAttribute(name, value) {
+    if (name.match(/^on([\s\S]+)$/)) {
+      console.log(RegExp.$1);
+    }
     this[name] = value;
+    this.props[name] = value;
   }
   appendChild(vchild) {
     this.children.push(vchild);
   }
-  mountTo(parent) {
+  mountTo(range) {
+    this.range = range;
+    this.update();
+    // let range = document.createRange();
+    // range.setStartAfter(parent.lastChild);
+    // range.setEndAfter(parent.lastChild);
+  }
+  update() {
+    let placeholder = document.createComment('placeholder');
+    let range = document.createRange();
+    range.setStart(this.range.endContainer, this.range.endOffset);
+    range.setEnd(this.range.endContainer, this.range.endOffset);
+    range.insertNode(placeholder);
+    this.range.deleteContents();
     const vdom = this.render();
-    vdom.mountTo(parent);
+    vdom.mountTo(this.range);
+  }
+  setState(state) {
+    const merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (typeof newState[p] === 'object' && newState[p] !== null) {
+          if (typeof oldState[p] !== 'object') {
+            oldState[p] = {};
+          }
+          merge(oldState[p], newState[p]);
+        } else {
+          oldState[p] = newState[p];
+        }
+      }
+    };
+    if (!this.state && state) {
+      this.state = {};
+    }
+    merge(this.state, state);
+    console.log(this.state)
+    this.update();
   }
 }
 
 export const ToyReact = {
   createElement(type, attributes, ...children) {
-    console.log(arguments);
-    console.log(children,'children')
     let element;
     if (typeof type === 'string') {
       element = new ElementWrapper(type);
@@ -53,11 +107,11 @@ export const ToyReact = {
     }
     const insertChildren = children => {
       for (let child of children) {
-        console.log(child,'dd')
         if (typeof child === 'object' && child instanceof Array) {
           insertChildren(child);
         } else {
-  
+          if (child === null || child === void 0)
+            child = '';
           if (!(child instanceof ElementWrapper)
                 && !(child instanceof TextWrapper)
                 && !(child instanceof Component))
@@ -74,6 +128,14 @@ export const ToyReact = {
   },
 
   render(vdom, element) {
-    vdom.mountTo(element);
+    let range = document.createRange();
+    if (element.children.length) {
+      range.setStartAfter(element.lastChild);
+      range.setEndAfter(element.lastChild);
+    } else {
+      range.setStart(element, 0);
+      range.setEnd(element, 0);
+    }
+    vdom.mountTo(range);
   }
 }
